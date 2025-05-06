b97d83399947d807c21974efb961fe6b5982d414
Davi1403 <davi.santos01010@gmail.com>
2025-05-06 15:34:41 -0300

    ListaDC

---
+++ ListaDC/src/NodeDC.java
@@ -0,0 +1,24 @@
+public class NodeDC {
+    private int x;
+    private NodeDC head, prox, ant;
+
+    public NodeDC(){
+        head = new NodeDC();
+    }
+
+    public void setX(int value){
+        x = value;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setAnt(NodeDC value){
+        
+    }
+
+
+
+
+}
